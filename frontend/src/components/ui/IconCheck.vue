62fe4b6db57e24556f1febc2b69f8a4816abcdf3
Nikita Kuprins <nikita.kuprins@gmail.com>
2026-06-17 23:59:09 +0300

    feat(frontend): add IconCheck and IconFail

---
+++ frontend/src/components/ui/IconCheck.vue
@@ -0,0 +1,14 @@
+<template>
+  <svg
+      viewBox="0 0 24 24"
+      aria-hidden="true"
+      class="size-full"
+      fill="none"
+      stroke="currentColor"
+      stroke-width="1.6"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+  >
+    <path d="M6.5 12.5l3.5 3.5 7.5 -7.5"/>
+  </svg>
+</template>
